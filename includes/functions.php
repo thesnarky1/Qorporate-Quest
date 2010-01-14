0ebeadf4410f907d3c343781c3de2ca1d1b9a4d2
snarky <snarky@mail.thesnarky.com>
2010-01-14 18:56:36 -0500

    Hellow world

---
+++ includes/functions.php
@@ -0,0 +1,15 @@
+<?php
+
+function render_header() {
+    echo "<html>\n";
+    echo "<head>\n";
+    echo "</head>\n";
+    echo "<body>\n";
+}
+
+function render_footer() {
+    echo "</body>\n";
+    echo "</html>\n";
+}
+
+?>
---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+
+    include('includes/functions.php');
+
+    render_header();
+
+    echo "Hello World!";
+
+    render_footer();
+
+
+
+?>
