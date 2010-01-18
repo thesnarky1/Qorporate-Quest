24d060b227326c846fa29f0db93b6840d413b77b
snarky <snarky@mail.thesnarky.com>
2010-01-18 09:03:29 -0500

    First revision for the login module

--- includes/login_functions.php
+++ includes/login_functions.php
@@ -15,6 +15,26 @@
       user_hash       (uniqie hash, used for authentication)
     */
 
+
+    //Login error most recently generated, will be overwritten upon new error
+    $current_login_error = false;
+
+    //Function to fetch the current login error for display
+    function get_login_error() {
+        global $current_login_error;
+
+        return $current_login_error;
+        $current_login_error = false;
+    }
+
+    //Function to set the current login error, will overwrite old data
+    function set_login_error($new_error) {
+        global $current_login_error;
+
+        $current_login_error = $new_error;
+    }
+
+    //Function to tell if someone is currently logged in or not
     function is_logged_in() {
         if(isset($_SESSION['user_name']) &&
            isset($_SESSION['user_id']) &&
@@ -28,17 +48,30 @@
         }
     }
 
+    //Function to handle logging in a user.
     function login_user($user_name, $user_pass) {
-        $query = "SELECT user_id, user_hash FROM users ".
-                 "WHERE user_name='$user_name' AND user_pass='$user_pass'";
+        $query = "SELECT user_id, user_hash, user_name FROM users ".
+                 "WHERE user_name LIKE '$user_name' AND user_pass='$user_pass'";
         if($user_row = mysqli_get_one($query)) {
             //Valid login
+            $user_name = $user_row['user_name'];
+            $user_hash = $user_rpw['user_hash'];
+            $user_id = $user_row['user_id'];
+            $_SESSION['user_name'] = $user_id;
+            $_SESSION['user_id'] = $user_id;
+            $_SESSION['user_hash'] = $user_hash;
+            return true;
         } else {
-            //Error
+            set_current_login_error("Invalid username/password combination");
+            return false;
         }
     }
 
+    //Function logs out the user by killing off SESSION
     function logout_user() {
+        $_SESSION['user_name'] = null;
+        $_SESSION['user_id'] = null;
+        $_SESSION['user_hash'] = null;
     }
 
 ?>
