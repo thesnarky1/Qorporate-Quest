15aca8ad1baaa077e156726738f15a29690973bc
snarky <snarky@mail.thesnarky.com>
2010-01-18 13:11:53 -0500

    Added safetify input method

--- login.php
+++ login.php
@@ -2,6 +2,28 @@
 
     include('includes/functions.php');
 
+    $error = false;
+
+    if(isset($_REQUEST['user_name']) && isset($_REQUEST['user_pass'])) {
+        //make our input nice and safe
+        $user_name = safetify_input($_REQUEST['user_name']);
+        $user_pass = safetify_input($_REQUEST['user_pass']);
+
+        //Check if this is a valid login
+        if(login_user($user_name, $user_pass)) {
+            //We're logged in, go home or where ever they came from
+            if(isset($_REQUEST['next_page'])) {
+                $next_page = safetify_input($_REQUEST['next_page']);
+                header("Location: $next_page");
+            } else {
+                header("Location: index.php");
+            }
+        } else {
+            //Display error
+            $error = get_login_error();
+        }
+    }
+
     render_header();
 
     echo "<div id='login_form'>\n";
@@ -13,6 +35,8 @@
     echo "<label class='fixed_width'>Password: </label>\n";
     echo "<input type='password' name='user_pass' />\n";
     echo "<br />\n";
+    echo "<input type='hidden' name='next_page' value='$_SERVER[PHP_SELF]' />\n";
+    echo "Remember login?<input type='checkbox' name='remember' value='true'/>\n";
     echo "<input type='submit' value='Login' />\n";
     echo "</form>\n";
     echo "</div> <!-- end login_form div -->\n";
