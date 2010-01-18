2c092cc244ff4cb5bd6aa6219bf1edefc824f91a
snarky <snarky@mail.thesnarky.com>
2010-01-18 15:13:53 -0500

    Started on initial landing page, have a sidebar and main text div now, need to come up with good flavor text for the front page.

--- index.php
+++ index.php
@@ -4,10 +4,23 @@
 
     render_header();
 
-    echo "<p>Hello World!</p>";
+    echo "<div id='sidebar'>\n";
+    echo "<div id='sidebar_stats'>\n";
+    echo "<h3>Statistics</h3>\n";
+    echo "<ul>\n";
+    echo "<li>Users: 1</li>\n";
+    echo "<li>Butt kicking: Infinite</li>\n";
+    echo "</ul>\n";
+    echo "</div> <!-- end sidebar_stats div -->\n";
+    echo "</div> <!-- end sidebar div -->\n";
+
+    echo "<div id='main_text'>\n";
+    echo "<h1>&lt;Company Name&gt;</h1>\n";
+    echo "<p>";
+    echo "Welcome to &lt;Company Name&gt; where we live for our employees! Your goal while employed at &lt;Company Name&gt; is to pursue your career goals, have fun, and most importantly, slave at your desk for 15 hours a day to make sure the TPS reports get out on time!";
+    echo "</p>\n";
+    echo "</div> <!-- end main_text div -->\n";
 
     render_footer();
-
-
 
 ?>
