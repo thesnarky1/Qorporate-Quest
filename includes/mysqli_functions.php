5674e35e7ed7df07501073495c5b0bfff5c5fdb1
snarky <snarky@mail.thesnarky.com>
2010-01-18 08:38:19 -0500

    Started on the database module, ripping my old mysqli_functions off of mafia

---
+++ includes/mysqli_functions.php
@@ -0,0 +1,74 @@
+<?php
+
+    /*
+      PHP Database mucking module
+      By Three Planets Software
+
+      Include at a global level, assumes a mysqli object named $dbh at the global level
+    */
+
+
+    function mysqli_get_one($query) {
+        global $dbh;
+        $result = mysqli_query($dbh, $query);
+        if($result && mysqli_num_rows($result) == 1) {
+            $row = mysqli_fetch_array($result);
+            return $row;
+        } else {
+            return false;
+        }
+    }
+
+    function mysqli_insert($query) {
+        global $dbh;
+        $result = mysqli_query($dbh, $query);
+        if($result && mysqli_affected_rows($dbh) == 1) {
+            return mysqli_insert_id($dbh);
+        } else {
+            return false;
+        }
+    }
+
+    function mysqli_set_many($query) {
+        global $dbh;
+        $result = mysqli_query($dbh, $query);
+        if($result && mysqli_affected_rows($dbh) > 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    
+    function mysqli_set_one($query) {
+        global $dbh;
+        $result = mysqli_query($dbh, $query);
+        if($result && mysqli_affected_rows($dbh) == 1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+    function mysqli_get_many($query) {
+        global $dbh;
+        $result = mysqli_query($dbh, $query);
+        if($result) {
+            $to_return = array();
+            while($row = mysqli_fetch_array($result)) {
+                $to_return[] = $row;
+            }
+            return $to_return;
+        }
+        return false;
+    }
+
+    function mysqli_delete($query) {
+        global $dbh;
+        $result = mysqli_query($dbh, $query);
+        if($result && mysqli_affected_rows($dbh) > 0) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+?>
