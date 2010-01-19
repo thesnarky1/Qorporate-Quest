7157ec95ee99f3b8379886c90a4b6b4cdfe2a890
snarky <snarky@mail.thesnarky.com>
2010-01-18 20:39:52 -0500

    Working registration script, still needs polishing for beta

--- register.php
+++ register.php
@@ -37,51 +37,78 @@
             $user_name = safetify_input($_REQUEST['user_name']);
         }
 
+        $user_hash = md5(uniqid('', TRUE));
+
         //Check if this is a valid login
         if($error == false) {
             //register them
+            $query = "INSERT INTO users(user_name, user_pass, user_hash, user_email) ".
+                     "VALUES('$user_name', MD5('$user_pass'), '$user_hash', '$user_email')";
+            $insert_id = mysqli_insert($query);
+            if($insert_id) {
+                //successful
+                login_user($user_name, $user_pass);
+                if(is_logged_in()) {
+                    //We're REALLY successful
+                } else {
+                    $error = "Unknown error, try again.";
+                }
+            } else {
+                //Todo: add better logic here to see WHY it failed
+                $error = "Error registering";
+            }
         }
     }
 
     render_header();
 
-    echo "<h1>Join &lt;Company Name&gt; Today!</h1>\n";
-    echo "<p>\n";
-    echo "Flavor text of job vacency.\n";
-    echo "</p>\n";
-
-    echo "<div id='login_form'>\n";
-    echo "<h3>Application</h3>\n";
-    echo "<form name='register' method='POST' action='register.php' class='form'>\n";
-    //Check for any issues with login
-    if($error != "") {
-        //Display our error nice and big
-        echo "<span class='error'>$error</span><br />\n";
+    if(is_logged_in()) {
+        if(isset($_REQUEST['submit'])) {
+            //Recent joining, let's greet them.
+            echo "<p>Welcome aboard, $user_name! Now get to work!</p>\n";
+        } else {
+            //Already employed, let's yell
+            echo "<p class='error'>YOU'RE ALREADY AN EMPLOYEE! GET BACK TO WORK!</p>\n";
+        }
+    } else {
+        echo "<h1>Join &lt;Company Name&gt; Today!</h1>\n";
+        echo "<p>\n";
+        echo "Flavor text of job vacency.\n";
+        echo "</p>\n";
+    
+        echo "<div id='login_form'>\n";
+        echo "<h3>Application</h3>\n";
+        echo "<form name='register' method='POST' action='register.php' class='form'>\n";
+        //Check for any issues with login
+        if($error != "") {
+            //Display our error nice and big
+            echo "<span class='error'>$error</span><br />\n";
+        }
+        echo "<label class='fixed_width'>Username: </label>\n";
+        echo "<input type='text' name='user_name' ";
+        if($user_name) {
+            echo "value='$user_name'";
+        }
+        echo "/>\n";
+        echo "<br />\n";
+        echo "<label class='fixed_width'>Email: </label>\n";
+        echo "<input type='text' name='user_email' ";
+        if($user_email) {
+            echo "value='$user_email'";
+        }
+        echo "/>\n";
+        echo "<br />\n";
+        echo "<label class='fixed_width'>Password: </label>\n";
+        echo "<input type='password' name='user_pass' />\n";
+        echo "<br />\n";
+        echo "<label class='fixed_width'>Pass again: </label>\n";
+        echo "<input type='password' name='user_pass_again' />\n";
+        echo "<br />\n";
+        echo "<input type='hidden' name='submit' value='submit' />\n";
+        echo "<input type='submit' value='Apply!' />\n";
+        echo "</form>\n";
+        echo "</div> <!-- end login_form div -->\n";
     }
-    echo "<label class='fixed_width'>Username: </label>\n";
-    echo "<input type='text' name='user_name' ";
-    if($user_name) {
-        echo "value='$user_name'";
-    }
-    echo "/>\n";
-    echo "<br />\n";
-    echo "<label class='fixed_width'>Email: </label>\n";
-    echo "<input type='text' name='user_email' ";
-    if($user_email) {
-        echo "value='$user_email'";
-    }
-    echo "/>\n";
-    echo "<br />\n";
-    echo "<label class='fixed_width'>Password: </label>\n";
-    echo "<input type='password' name='user_pass' />\n";
-    echo "<br />\n";
-    echo "<label class='fixed_width'>Pass again: </label>\n";
-    echo "<input type='password' name='user_pass_again' />\n";
-    echo "<br />\n";
-    echo "<input type='hidden' name='submit' value='submit' />\n";
-    echo "<input type='submit' value='Apply!' />\n";
-    echo "</form>\n";
-    echo "</div> <!-- end login_form div -->\n";
 
     render_footer();
 
