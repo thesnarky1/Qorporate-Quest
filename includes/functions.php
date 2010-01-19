2cf638ecf0893427b88c6304c5fcab974b9e9627
snarky <snarky@mail.thesnarky.com>
2010-01-18 22:07:42 -0500

    Added get_characters function to return all characters a user has

--- includes/functions.php
+++ includes/functions.php
@@ -8,4 +8,11 @@
     include('login_functions.php');
     include('site_functions.php');
 
+    //Function takes a user_id and returns an array of all the characters that belong to them
+    function get_characters($user_id) {
+        $query = "SELECT * FROM characters WHERE user_id='$user_id'";
+        $characters = mysqli_get_many($query);
+        return $characters;
+    }
+
 ?>
