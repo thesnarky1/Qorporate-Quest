fcbb7c2e1ce3a444e804acedfd7b82f9922222d0
snarky <snarky@mail.thesnarky.com>
2010-01-30 20:46:56 -0500

    Added functions to pull a specified task from the task list and toss it into our current task area, as well as a function to grab the top task off the list

--- scripts/play.js
+++ scripts/play.js
@@ -9,3 +9,16 @@ function toggle_my_p(div) {
         div.css("display", "block");
     }
 }
+
+function do_task(div) {
+    var current_quest = $("#char_quest_current_text");
+    current_quest.html(div.html());
+    if(current_quest.parent().css("display") != "block") {
+        current_quest.parent().css("display", "block");
+    }
+    div.remove();
+}
+
+function pick_next_task() {
+    do_task($($('#char_quests_div').children('#char_quest_single')[0]));
+}
