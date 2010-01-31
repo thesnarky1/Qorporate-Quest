d16cc02d4dceecdea9c630e1ef90618533ef4aa9
snarky <snarky@mail.thesnarky.com>
2010-01-31 00:38:32 -0500

    Added code to check for turning in a given quest

--- tasks.php
+++ tasks.php
@@ -12,27 +12,52 @@
     
             //check for valid char
             if(user_owns_character($user_id, $char_id, $user_hash)) {
-                $to_return = "";
-                $tasks = get_character_tasks($char_id, 10);
-                foreach($tasks as $task) {
-                    //Set up vars
-                    $quest_name = $task['quest_name'];
-                    $quest_flavor = $task['quest_flavor'];
-                    $quest_experience = $task['adventure_experience'];
 
-                    //Display
-                    $to_return .= "<div id='char_quest_single'>\n";
-                    $to_return .= "<div id='char_quest_single_head' onclick='toggle_my_p(this);'>\n";
-                    $to_return .= "<h3>$quest_name</h3>\n";
-                    $to_return .= "<span>Experience: $quest_experience</span>\n";
-                    $to_return .= "</div> <!-- end char_quest_single_head -->\n";
-                    $to_return .= "<p>$quest_flavor</p>\n";
-                    $to_return .= "</div> <!-- end char_quest_single -->\n";
+                //If a quest id is given its being given to complete the quest
+                if(isset($_REQUEST['quest_id'])) {
+                    $adventure_id = $_REQUEST['quest_id'];
+                    $to_return = "";
+
+                    $query = "SELECT adventure_experience FROM adventures ".
+                             "WHERE adventure_id=? AND character_id=? ".
+                             "ORDER BY adventure_id LIMIT 1";
+                    $result = $conn->GetRow($query, array($adventure_id, $char_id));
+                    if($result) {
+                        //We have a valid quest
+                        //add the experience
+                        $exp = $result['adventure_experience'];
+                        $to_return = add_character_experience($char_id, $exp);
+                        //Remove the quest
+                        $query = "DELETE FROM adventures ".
+                                 "WHERE adventure_id=? ".
+                                 "LIMIT 1";
+                        $result = $conn->Execute($query, array($adventure_id));
+                    }
+                    echo json_encode($to_return);
+                } else {
+                    $to_return = "";
+                    $tasks = get_character_tasks($char_id, 10);
+                    foreach($tasks as $task) {
+                        //Set up vars
+                        $quest_name = $task['quest_name'];
+                        $quest_flavor = $task['quest_flavor'];
+                        $quest_experience = $task['adventure_experience'];
+    
+                        //Display
+                        $to_return .= "<div id='char_quest_single'>\n";
+                        $to_return .= "<div id='char_quest_single_head' onclick='toggle_my_p(this);'>\n";
+                        $to_return .= "<h3>$quest_name</h3>\n";
+                        $to_return .= "<span>Experience: $quest_experience</span>\n";
+                        $to_return .= "</div> <!-- end char_quest_single_head -->\n";
+                        $to_return .= "<p>$quest_flavor</p>\n";
+                        $to_return .= "</div> <!-- end char_quest_single -->\n";
+                    }
+                    echo json_encode(array("return_value" => $to_return));
                 }
-                echo json_encode(array("return_value" => $to_return));
+            } else {
+                echo json_encode(array("return_value" => "<p class='error'>You a cheater</p>\n"));
             }
         }
-
     }
 
 
