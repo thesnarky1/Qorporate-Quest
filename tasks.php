ea9f3373647767d7c36c3de28ee41a854007b4f5
snarky <snarky@mail.thesnarky.com>
2010-02-02 22:56:26 -0500

    Most of the work done transforming tasks.php to use the tasks table, rather than the adventures table

--- tasks.php
+++ tasks.php
@@ -13,29 +13,32 @@
             //check for valid char
             if(user_owns_character($user_id, $char_id, $user_hash)) {
 
-                //If a quest id is given its being given to complete the quest
-                if(isset($_REQUEST['quest_id'])) {
-                    $adventure_id = $_REQUEST['quest_id'];
+                //If a task id is given its being given to complete the task
+                if(isset($_REQUEST['task_id'])) {
+                    $task_id = $_REQUEST['task_id'];
                     $to_return = "";
 
-                    $query = "SELECT adventure_experience FROM adventures ".
-                             "WHERE adventure_id=? AND character_id=? ".
-                             "ORDER BY adventure_id LIMIT 1";
-                    $result = $conn->GetRow($query, array($adventure_id, $char_id));
+                    $query = "SELECT task_experience FROM tasks ".
+                             "WHERE task_id=? AND character_id=? ".
+                             "ORDER BY task_id LIMIT 1";
+                    $result = $conn->GetRow($query, array($task_id, $char_id));
                     if($result) {
                         //We have a valid quest
                         //add the experience
-                        $exp = $result['adventure_experience'];
+                        $exp = $result['task_experience'];
                         $to_return = add_character_experience($char_id, $exp);
                         //Remove the quest
-                        $query = "DELETE FROM adventures ".
-                                 "WHERE adventure_id=? ".
+                        $query = "DELETE FROM tasks ".
+                                 "WHERE task_id=? ".
                                  "LIMIT 1";
-                        $result = $conn->Execute($query, array($adventure_id));
+                        $result = $conn->Execute($query, array($task_id));
                         echo json_encode($to_return);
                     } else {
                         $error = $conn->ErrorMsg();
-                        echo json_encode(array("return_value" => "$error - $query - $adventure_id - $char_id"));
+                        if(!$error || $error == "") {
+                            $error = "No Mysql error....";
+                        }
+                        echo json_encode(array("return_value" => "$error - $query - $task_id - $char_id"));
                     }
                 } else {
                     $to_return = "";
@@ -44,14 +47,14 @@
                         //Set up vars
                         $quest_name = $task['quest_name'];
                         $quest_flavor = $task['quest_flavor'];
-                        $quest_experience = $task['adventure_experience'];
-                        $adventure_id = $task['adventure_id'];
+                        $quest_experience = $task['task_experience'];
+                        $task_id = $task['task_id'];
                         //Display
                         $to_return .= "<div id='char_quest_single'>\n";
                         $to_return .= "<div id='char_quest_single_head' onclick='toggle_my_p(this);'>\n";
                         $to_return .= "<h3>$quest_name</h3>\n";
                         $to_return .= "<span>Experience: $quest_experience</span>\n";
-                        $to_return .= "<div id='char_quest_single_id'>$adventure_id</div>\n";
+                        $to_return .= "<div id='char_quest_single_id'>$task_id</div>\n";
                         $to_return .= "</div> <!-- end char_quest_single_head -->\n";
                         $to_return .= "<p>$quest_flavor</p>\n";
                         $to_return .= "</div> <!-- end char_quest_single -->\n";
