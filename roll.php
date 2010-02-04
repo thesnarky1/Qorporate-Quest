d9c425183959c51c87126edf5988cd9b320e3880
snarky <snarky@mail.thesnarky.com>
2010-02-03 23:29:55 -0500

    Moved roll function to includes/character_functions.php

--- roll.php
+++ roll.php
@@ -2,9 +2,11 @@
 
     include('includes/functions.php');
 
-
-
-
+    if(is_logged_in()) {
+        $user_id = get_logged_in_user();
+        $roll = roll($user_id);
+        echo json_encode($roll);
+    }
 
 
 
