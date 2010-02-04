f30c76373e9c57664375a7e6e2c2d280ece41abe
snarky <snarky@mail.thesnarky.com>
2010-02-04 00:27:45 -0500

    Roll will spit back our AJAX for stat rerolling

--- roll.php
+++ roll.php
@@ -3,7 +3,7 @@
     include('includes/functions.php');
 
     if(is_logged_in()) {
-        $user_id = get_logged_in_user();
+        $user_id = get_logged_in_userid();
         $roll = roll($user_id);
         echo json_encode($roll);
     }
