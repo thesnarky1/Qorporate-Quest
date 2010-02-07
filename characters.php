90a5bca1c6b6c95ec6d80b28b70de155dd326a9b
snarky <snarky@mail.thesnarky.com>
2010-02-07 13:54:38 -0500

    Added boss bit on the sidebar so one can view boss details

--- characters.php
+++ characters.php
@@ -4,6 +4,52 @@
 
     render_header();
 
+    //
+    // Sidebar
+    //
+    echo "<div id='sidebar'>\n";
+    $rand_characters = get_random_characters(5);
+    if($rand_characters) {
+        echo "<div id='sidebar_stats'>\n";
+        echo "<h3>Random Characters</h3>\n";
+        echo "<ul>\n";
+        foreach($rand_characters as $character_row) {
+            $char_name = $character_row['character_name'];
+            $char_id = $character_row['character_id'];
+            echo "<li><a href='characters.php?char_id=$char_id'>$char_name</a></li>\n";
+        }
+        echo "</ul>\n";
+        echo "</div> <!-- end sidebar_stats div -->\n";
+    }
+    $rand_characters = get_random_characters(5, "character_level DESC");
+    if($rand_characters) {
+        echo "<div id='sidebar_stats'>\n";
+        echo "<h3>Top Characters</h3>\n";
+        echo "<ul>\n";
+        foreach($rand_characters as $character_row) {
+            $char_name = $character_row['character_name'];
+            $char_id = $character_row['character_id'];
+            $char_level = $character_row['character_level'];
+            echo "<li><a href='characters.php?char_id=$char_id'>$char_name ($char_level)</a></li>\n";
+        }
+        echo "</ul>\n";
+        echo "</div> <!-- end sidebar_stats div -->\n";
+    }
+    echo "<div id='sidebar_boss'>\n";
+    echo "<h3>Boss Facts</h3>\n";
+    echo "<span id='boss_name'></span>\n";
+    echo "<br />\n";
+    echo "<br />\n";
+    echo "<span>Experience Bonus:</span>\n";
+    echo "<span id='boss_experience'></span>\n";
+    echo "<br />\n";
+    echo "<p id='boss_flavor'></p>\n";
+    echo "</div> <!-- end sidebar_boss div -->\n";
+    echo "</div> <!-- end sidebar div -->\n";
+    //
+    // End sidebar
+    //
+
     if(isset($_REQUEST['char_id'])) {
         //Display Character info
 
