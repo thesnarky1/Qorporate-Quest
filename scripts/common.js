942c5c0ba47826afb6cd2de6078e5029a557e6ea
snarky <snarky@mail.thesnarky.com>
2010-02-07 23:46:41 -0500

    Added registration form verification

--- scripts/common.js
+++ scripts/common.js
@@ -1,3 +1,14 @@
+$(document).ready(function() {
+    if($('#register_form')) {
+        $('input[type=submit]').bind('click', function(e) {
+                                                  validate_registration_form();
+                                              });
+        $('#register_form').bind('submit', function(e) {
+                                                           e.preventDefault();
+                                                       });
+    }
+});
+
 function swap_text(final_div, initial_div) {
     var content = $(initial_div).html();
     var final_div_inner = final_div + " > p";
@@ -39,4 +50,31 @@ function swap_boss(link) {
 function clear_text(final_div) {
     $(final_div).css("display", "none");
     $(final_div).html("");
+}
+
+//Function to validate the registration form
+function validate_registration_form() {
+    var user_name = $('#register_user_name').val();
+    var user_email = $('#register_user_email').val();
+    var user_pass = $('#register_user_pass').val();
+    var user_pass2 = $('#register_user_pass2').val();
+
+    user_name = $.trim(user_name);
+    user_email = $.trim(user_email);
+    user_pass = $.trim(user_pass);
+    user_pass2 = $.trim(user_pass2);
+
+    if(!user_name || user_name == "") {
+        $('#register_error').css('display', 'block').html("You must fill in a username.");
+    } else if(!user_email || user_email == "") {
+        $('#register_error').css('display', 'block').html("You must fill in an email.");
+    } else if(!user_pass || user_pass == "") {
+        $('#register_error').css('display', 'block').html("You must fill in a password.");
+    } else if(!user_pass2 || user_pass2 == "" || user_pass2 != user_pass) {
+        $('#register_error').css('display', 'block').html("You must fill in the same password twice.");
+    } else {
+        alert("Good to go");
+        $('#register_form').unbind('submit');
+        $('#register_form').submit();
+    }
 }
