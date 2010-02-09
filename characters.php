b60ad9979ee417a1979b44a3bd651a051c0e608f
snarky <snarky@mail.thesnarky.com>
2010-02-09 09:44:13 -0500

    Cleaned up some comments, added list of user's characters if no character is specified

--- characters.php
+++ characters.php
@@ -65,16 +65,43 @@
         $character_row = $conn->GetRow($query, array($char_id));
         if($character_row) {
             render_character_sheet($char_id);
-//            $char_name = $character_row['character_name'];
-//            $department_name = $character_row['department_name'];
-//            $department_id = $character_row['department_id'];
-//            $job_name = $character_row['job_name'];
-//            $job_id = $character_row['job_id'];
-//            echo "<h3>$char_name</h3>\n";
-//            echo "<p>$char_name is a <a href='characters.php?job_id=$job_id'>$job_name</a> in the <a href='characters.php?department_id=$department_id'>$department_name</a> department.</p>\n";
         }
     } else {
-        //Dsplay character picking stuffs
+        echo "<div id='main_text'>\n";
+        echo "<h3>Characters</h3>\n";
+        $user_id = get_logged_in_userid();
+        $characters = get_characters($user_id, "characters.character_level DESC");
+        if($characters && count($characters) > 0) {
+            echo "<div id='user_chars'>\n";
+            echo "<span>Your Characters:</span>\n";
+            echo "<table>\n";
+            echo "<thead>\n";
+            echo "<td class='td_name'>Name</td>\n";
+            echo "<td class='td_level'>Level</td>\n";
+            echo "<td class='td_job'>Job</td>\n";
+            echo "<td class='td_dept'>Department</td>\n";
+            echo "</thead>\n";
+            foreach($characters as $character) {
+                $char_name = $character['character_name'];
+                $char_id = $character['character_id'];
+                $char_level = $character['character_level'];
+                $char_job = $character['job_name'];
+                $char_department = $character['department_name'];
+                echo "<tr>\n";
+                echo "<td><a href='characters.php?char_id=$char_id'>$char_name</a></td>\n";
+                echo "<td>$char_level</td>\n";
+                echo "<td>$char_job</td>\n";
+                echo "<td>$char_department</td>\n";
+                echo "</tr>\n";
+            }
+            echo "</table>\n";
+            echo "</div> <!-- end user_chars div -->\n";
+        } else {
+            echo "<div id='user_chars'>\n";
+            echo "<p>$user_name has no characters yet.</p>\n";
+            echo "</div> <!-- end user_chars div -->\n";
+        }
+        echo "</div> <!-- end main_text div -->\n";
     }
 
     render_footer();
