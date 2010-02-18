d7a7d674d86f3bb8a52c0a88566e2e1076c2ed0b
snarky <snarky@mail.thesnarky.com>
2010-02-18 16:44:21 -0500

    Added password to sidebar

--- password.php
+++ password.php
@@ -8,29 +8,29 @@
 
     if(isset($_REQUEST['reset_pass'])) {
         //deal with password reset
-    } else if(isset($_REQUEST['change_email'])) {
-        //change their email
-        //stupidly assuming everything came through correctly
-        $old_pass = $_REQUEST['curr_pass'];
-        $new_email = $_REQUEST['new_email'];
-        $new_email2 = $_REQUEST['new_email2'];
-
-        if(trim($old_pass) != "" && trim($new_email) != "" &&
-           trim($new_email2) != "" && $new_email == $new_email2) {
-            $user_id = get_logged_in_userid();
-            $query = "SELECT user_id FROM users WHERE user_id=? AND user_pass=MD5(?)";
-            $real_user = $conn->Execute($query, array($user_id, $old_pass));
-            if($real_user && $real_user->RowCount() == 1) {
-                //valid user, update
-                $query = "UPDATE users SET user_email=? WHERE user_id=?";
-                $conn->Execute($query, array($new_email, $user_id));
-                $change_email_error = "Email changed successfully to $new_email";
-            } else {
-                $change_email_error = "Invalid current password given.";
-            }
-        } else {
-            $change_email_error = "Please ensure you fill in all required fields with non whitespace characters";
-        }
+//    } else if(isset($_REQUEST['change_email'])) {
+//        //change their email
+//        //stupidly assuming everything came through correctly
+//        $old_pass = $_REQUEST['curr_pass'];
+//        $new_email = $_REQUEST['new_email'];
+//        $new_email2 = $_REQUEST['new_email2'];
+//
+//        if(trim($old_pass) != "" && trim($new_email) != "" &&
+//           trim($new_email2) != "" && $new_email == $new_email2) {
+//            $user_id = get_logged_in_userid();
+//            $query = "SELECT user_id FROM users WHERE user_id=? AND user_pass=MD5(?)";
+//            $real_user = $conn->Execute($query, array($user_id, $old_pass));
+//            if($real_user && $real_user->RowCount() == 1) {
+//                //valid user, update
+//                $query = "UPDATE users SET user_email=? WHERE user_id=?";
+//                $conn->Execute($query, array($new_email, $user_id));
+//                $change_email_error = "Email changed successfully to $new_email";
+//            } else {
+//                $change_email_error = "Invalid current password given.";
+//            }
+//        } else {
+//            $change_email_error = "Please ensure you fill in all required fields with non whitespace characters";
+//        }
     } else if(isset($_REQUEST['change_password'])) {
         //change their password
         //stupidly assuming everything came through correctly
@@ -69,7 +69,7 @@
         $rand_char = $characters[$rand_index];
         echo "<div id='sidebar'>\n";
         echo "<div id='sidebar_stats'>\n";
-        echo "<h3>Stats</h3>\n";
+        echo "<h3>Password Stats</h3>\n";
         echo "<ul>\n";
         echo "<li>Average length: 6</li>\n";
         echo "<li>Commonest letter: $rand_char</li>\n";
@@ -102,28 +102,28 @@
         echo "</div> <!-- end char_creation div -->\n";
 
         //Change email form
-        echo "<div id='char_creation'>\n";
-        echo "<h3>Change Email</h3>\n";
-        echo "<div id='change_email_error' style='text-align: center; color: red;'>";
-        if($change_email_error) {
-            echo $change_email_error;
-        }
-        echo "</div> <!-- end change_email_error div -->\n";
-        echo "<form action='password.php' id='change_email_form' method='POST'>\n";
-        echo "<label class='fixed_width'>Current Password: </label>";
-        echo "<input type='password' id='change_email_pass' name='curr_pass' />\n";
-        echo "<br />\n";
-        echo "<label class='fixed_width'>New Email: </label>";
-        echo "<input type='text' id='change_email_new' name='new_email' />\n";
-        echo "<br />\n";
-        echo "<label class='fixed_width'>Repeat Email: </label>";
-        echo "<input type='text' id='change_email_new2' name='new_email2' />\n";
-        echo "<br />\n";
-        echo "<label class='fixed_width'></label>";
-        echo "<input type='submit' id='change_email_submit' name='change_email' value='Change email' />\n";
-        echo "</form>\n";
-
-        echo "</div> <!-- end char_creation div -->\n";
+//        echo "<div id='char_creation'>\n";
+//        echo "<h3>Change Email</h3>\n";
+//        echo "<div id='change_email_error' style='text-align: center; color: red;'>";
+//        if($change_email_error) {
+//            echo $change_email_error;
+//        }
+//        echo "</div> <!-- end change_email_error div -->\n";
+//        echo "<form action='password.php' id='change_email_form' method='POST'>\n";
+//        echo "<label class='fixed_width'>Current Password: </label>";
+//        echo "<input type='password' id='change_email_pass' name='curr_pass' />\n";
+//        echo "<br />\n";
+//        echo "<label class='fixed_width'>New Email: </label>";
+//        echo "<input type='text' id='change_email_new' name='new_email' />\n";
+//        echo "<br />\n";
+//        echo "<label class='fixed_width'>Repeat Email: </label>";
+//        echo "<input type='text' id='change_email_new2' name='new_email2' />\n";
+//        echo "<br />\n";
+//        echo "<label class='fixed_width'></label>";
+//        echo "<input type='submit' id='change_email_submit' name='change_email' value='Change email' />\n";
+//        echo "</form>\n";
+//
+//        echo "</div> <!-- end char_creation div -->\n";
     } else {
         //Must be a password reset request
         echo "<div id='char_creation'>\n";
